d2df724b6925b91c8fa3296b3d1209d9a0c8deb7
tinyxiao <xjw87610@gmail.com>
2018-11-21 22:50:52 +0800

    create login view

---
+++ datavisual-ui/src/components/login/Login.vue
@@ -0,0 +1,52 @@
+<template>
+
+  <el-container>
+    <el-main>
+      <el-card>
+        <el-form label-width="80px">
+          <el-form-item label="用户名">
+            <el-input type="text"></el-input>
+          </el-form-item>
+
+          <el-form-item label="密码">
+            <el-input type="password"></el-input>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary">登录</el-button>
+            <el-button type="reset">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </el-main>
+
+    <el-footer>
+      copyright
+    </el-footer>
+  </el-container>
+</template>
+
+<style>
+
+  .el-container {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: aliceblue;
+  }
+
+  .el-main {
+    margin-left: 30%;
+    margin-top: 10%;
+    width: 450px;
+    height: 300px;
+  }
+</style>
+
+<script>
+  export default{
+    data () {
+      return {msg: '登录页'}
+    }
+  }
+</script>
--- datavisual-ui/src/router/index.js
+++ datavisual-ui/src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ElementUI from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login/Login'
 
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
   routes: [
@@ -10,6 +14,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/login',
+      name: "Login",
+      component: Login
     }
   ]
 })
